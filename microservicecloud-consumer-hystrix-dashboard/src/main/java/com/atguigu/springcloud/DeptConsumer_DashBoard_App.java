559cd2aa833a2b6328eddeaf75eaffab6a033967
郑超 <1658955813@qq.com>
2021-05-23 21:59:26 +0800

    springCloud+DashBoard 微服务的图形化监控 2021年5月23日 郑超

---
+++ microservicecloud-consumer-hystrix-dashboard/src/main/java/com/atguigu/springcloud/DeptConsumer_DashBoard_App.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author 郑超
+ * @date 2021-05-23 21:39
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class DeptConsumer_DashBoard_App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConsumer_DashBoard_App.class, args);
+    }
+
+}
