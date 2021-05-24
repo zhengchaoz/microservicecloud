ffae5111ab64d14193ffa262ce5905b51df65d9f
郑超 <1658955813@qq.com>
2021-05-24 22:41:28 +0800

    springCloud集中式的配置文件管理，完成了服务端和GitHub上的配置文件共同联调 2021年5月24日 郑超

---
+++ microservicecloud-config-3344/src/main/java/com/atguigu/springcloud/Config_3344_StartSpringCloudApp.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author 郑超
+ * @date 2021-05-24 22:11
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class Config_3344_StartSpringCloudApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Config_3344_StartSpringCloudApp.class, args);
+    }
+
+}
