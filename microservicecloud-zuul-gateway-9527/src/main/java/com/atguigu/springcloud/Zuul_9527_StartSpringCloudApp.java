51e73ec75603b7fb4e80acafcb9d8f3dc05b0da1
郑超 <1658955813@qq.com>
2021-05-24 21:24:02 +0800

    springCloud+zuul 统一网关设置 2021年5月24日 郑超

---
+++ microservicecloud-zuul-gateway-9527/src/main/java/com/atguigu/springcloud/Zuul_9527_StartSpringCloudApp.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author 郑超
+ * @date 2021-05-24 21:03
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class Zuul_9527_StartSpringCloudApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zuul_9527_StartSpringCloudApp.class, args);
+    }
+
+}
