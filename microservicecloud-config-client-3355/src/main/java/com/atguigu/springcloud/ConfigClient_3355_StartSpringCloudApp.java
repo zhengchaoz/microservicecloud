99c9aea1ac0adfff4c2a9a1cc8ff1b1c515ffdaa
郑超 <1658955813@qq.com>
2021-05-25 23:18:36 +0800

    springCloud+config 多个微服务统一从GitHub上拉取配置，本次是client客户端测试，通过server服务端配置GitHub消息获取配置 2021年5月25日 郑超

---
+++ microservicecloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClient_3355_StartSpringCloudApp.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 郑超
+ * @date 2021-05-25 22:16
+ */
+@SpringBootApplication
+public class ConfigClient_3355_StartSpringCloudApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient_3355_StartSpringCloudApp.class, args);
+    }
+
+}
